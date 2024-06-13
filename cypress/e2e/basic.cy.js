d6868d160b06a3b1eca38776051c0cacc809a2bd
NekoStalker <noghri@mail.ru>
2024-06-13 12:57:46 +0300

    add cy test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -11,16 +11,48 @@ describe("Drag and Drop Functionality", () => {
       // Wait for the first ingredient to load
       cy.wait(1000);
 
+
+      cy.get(appSelectors.ingredients.main).trigger('click');
+      cy.wait(1000);
+      cy.get(appSelectors.modal.ingredientName).should('not.be.empty');
+      cy.get(appSelectors.modal.close).trigger('click');
       // Найти первый элемент с атрибутом data-cy="burger-ingredient-bun"
-      cy.get(appSelectors.ingredients.ingredientBun).first().trigger('dragstart');
-      cy.get(appSelectors.constructor.constructorContainer).should('exist').trigger('drop');
+      cy.get(appSelectors.ingredients.bun).first().trigger('dragstart');
+      cy.get(appSelectors.constructor.container).should('exist').trigger('drop');
 
-      cy.get(appSelectors.constructor.topBun).should('exist');
-      cy.get(appSelectors.constructor.bottomBun).should('exist');
+      // cy.get(appSelectors.constructor.topBun).should('exist');
+      // cy.get(appSelectors.constructor.bottomBun).should('exist');
 
-      cy.get(appSelectors.ingredients.bun).first().trigger('dragstart'); // Пример использования другого типа ингредиента
-      cy.get(appSelectors.constructor.constructorContainer).trigger('drop');
+      cy.get(appSelectors.ingredients.main).first().trigger('dragstart'); // Пример использования другого типа ингредиента
+      cy.get(appSelectors.constructor.container).trigger('drop');
+      cy.get(appSelectors.ingredients.sauce).first().trigger('dragstart'); 
+      cy.get(appSelectors.constructor.container).trigger('drop');
       cy.get(appSelectors.constructor.burgerItems).should('not.be.empty');
+
+      cy.get(appSelectors.constructor.sendOrder).trigger('click');
+      cy.wait(1000);
+      const email = 'drevan777@gmail.com';
+      const password = 'Avatar007';
+
+      // Получение поля ввода email и ввод email
+      cy.get(appSelectors.login.emailField)
+        .should('be.visible')
+        .type(email);
+
+      // Получение поля ввода пароля и ввод пароля
+      cy.get(appSelectors.login.passwordField)
+        .should('be.visible')
+        .type(password);
+
+      // Нажатие на кнопку входа
+      cy.get(appSelectors.login.loginButton)
+        .should('be.visible')
+        .click();
+      cy.wait(1000);
+      cy.get(appSelectors.constructor.sendOrder).trigger('click');
+      cy.wait(16000);
+      cy.get(appSelectors.modal.orderNumber).should('exist');
+      cy.get(appSelectors.modal.close).trigger('click');
     });
   });
 });
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,25 +2,27 @@ import './commands';
 
 export const appSelectors = {
     ingredients: {
-        bun: '[data-cy="burger-ingredient-bun"]',
-        main: '[data-cy="burger-ingredient-main"]',
-        sauce: '[data-cy="burger-ingredient-sauce"]',
+        bun: '[data-cy=burger-ingredient-bun]',
+        main: '[data-cy=burger-ingredient-main]',
+        sauce: '[data-cy=burger-ingredient-sauce]',
       },
       constructor: {
-        container: '[data-cy="constructor-ingredients-container"]',
-        topBun: '[data-cy="constructor-bun-top"]',
-        bottomBun: '[data-cy="constructor-bun-bottom"]',
-        items: '[data-cy="constructor-ingredients-container"]',
+        container: '[data-cy=constructor-ingredients-container]',
+        topBun: '[data-cy=constructor-bun-top]',
+        bottomBun: '[data-cy=constructor-bun-bottom]',
+        items: '[data-cy=constructor-ingredients-container]',
+        sendOrder:  '[data-cy=send-order]',
       },
     login: {
-        emailField: '[data-cy="email"]',
-        passwordField: '[data-cy="password"]',
-        loginButton: '[data-cy="login-button"]'
+        emailField: '[data-cy=email]',
+        passwordField: '[data-cy=password]',
+        loginButton: '[data-cy=login-button]'
     },
     modal: {
-       orderNumber: '[data-cy="order-number"]',
-       close: '[data-cy="close-button"]',
-       orderNumber: '[data-cy="order-number"]',
+       orderNumber: '[data-cy=order-number]',
+       close: '[data-cy=close-button]',
+       orderNumber: '[data-cy=order-number]',
+       ingredientName: '[data-cy=ingredient-name]'
     }
 };
 
